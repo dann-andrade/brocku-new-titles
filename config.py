e53b0d9afc1eec1ef9147cf8c0ac9febd2495ab4
Daniel Andrade <dandrade@brocku.ca>
2022-02-13 16:08:42 -0500

    Added config file

---
+++ config.py
@@ -0,0 +1,43 @@
+#-----------------------------------------------------------------------------------------------
+#
+#           Institution Specific Settings
+#
+#-----------------------------------------------------------------------------------------------
+
+#Base API component (used for all API calls)
+baselink = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1'
+
+#API Keys
+
+#Alma
+almaKey = '&apikey='
+
+#Google
+googKey = ''
+
+#Analytics Report Path
+reportPath = '/analytics/reports?path=%2Fshared%2FBrock+University+01OCUL_BU%2FReports%2FDanAndrade%2FNewBooksList%2F'
+
+#Report Names
+physicalReport = 'NewPBooksExpanded'
+electronicReport = 'NewEBooksExpanded'
+
+#Set IDs
+addPhysicalSet = '2685575960005152'
+removePhysicalSet = '2685576080005152'
+addElectronicSet = '2685577380005152'
+removeElectronicSet = '2685577430005152'
+
+#Collection IDs
+physicalCollectionID = '81123114400005152'
+electronicCollectionID = '81123466820005152'
+
+#Collection Names
+physicalCollection = 'New Titles in Print'
+electronicCollection = 'New Ebook Titles'
+
+#Job Names
+addPhysicalCollectionJob = 'Add Titles to Collection - via API - New Electronic Titles (Add)'
+removePhysicalCollectionJob = 'Remove Titles from Collection - via API - New Electronic Titles (Remove)'
+addElectronicCollectionJob = 'Add Titles to Collection - via API - New Electronic Titles (Add)'
+removeElectronicCollectionJob = 'Remove Titles from Collection - via API - New Electronic Titles (Remove)'
