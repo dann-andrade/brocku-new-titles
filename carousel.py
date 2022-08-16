e98caed1778310f3180b95e9aff12bd069bf6abe
Daniel Andrade <dandrade@brocku.ca>
2022-08-16 16:37:25 -0400

    Added functionality to format data for carousel

---
+++ carousel.py
@@ -0,0 +1,58 @@
+from fileinput import filename
+import requests
+import json
+from os import path
+
+
+#-----------------------------------------------------------------------------------------------
+#
+#           Build json for visual elements
+#
+#           - Assembles array for embedded Omni book carousel
+#           - Disregards books with no isbn
+#           - Filters default covers by 86kb filesize
+#           - Pulls only first 500 results, for efficiency
+#           - Cover URL is https://syndetics.com/index.php?client=primo&isbn=<<INSERT ISBN>>/mc.jpg
+#
+#-----------------------------------------------------------------------------------------------
+
+goodtitles = []
+
+def checkCovers(filename):
+
+    count = 0
+
+    with open(filename) as pfile:
+        ptitles = json.load(pfile)
+        
+        for ptitle in ptitles['titles']:
+            
+            isbns = ptitle['isbn'].split(';')
+            minSize = 86
+
+            for isbn in isbns:
+
+                if isbn != 'n/a':
+
+                    imgURL = 'https://syndetics.com/index.php?client=primo&amp&isbn=' + isbn + '/mc.jpg'
+                    size = int(requests.get(imgURL, stream= True).headers['Content-length'])
+
+                    if size > minSize:
+                        bestISBN = isbn
+                        minSize = size
+
+            if minSize > 86:
+                ptitle['isbn'] = bestISBN
+                goodtitles.append(ptitle)
+                count += 1
+                if count >= 500:
+                    break 
+
+            
+
+checkCovers('ptitles.json')
+checkCovers('etitles.json')
+
+with open('gtitles.json', 'w') as gfile:
+    gfile.write(json.dumps(goodtitles, indent=4, sort_keys=True))
+
