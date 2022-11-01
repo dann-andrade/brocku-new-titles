ce1acd287bc3619be39b8147934f4a84709e1f65
root <root@localhost>
2022-11-01 14:51:39 +0000

    Remove routing to simplify app

--- web/js/app.js
+++ web/js/app.js
@@ -3,23 +3,98 @@
   'use strict';
 
   angular
-    .module('newTitles', [
-      'ngRoute',
-      'myControllers'
-    ])
-    .config(config);
+    .module('newTitles', [])
+    .controller('MainController',
+		function ($http) {
 
-  function config($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'mainController',
-        controllerAs: '$ctrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }
+			const $ctrl = this;
+			const scrollCont = document.getElementById('bu-outer-carousel');
+			const innerCar = document.getElementById('bu-inner-carousel');
+
+      scrollCont.scrollLeft = 0;
+
+			var loadCursor = 0;
+      var dispCursor = 0;
+			var screenWidth = 0;
+
+			$ctrl.newbooks = [];
+			$ctrl.display = [];
+			$ctrl.dataLoaded = false;
+
+			$http.get('http://rtod.library.brocku.ca:8080/gtitles.json').then(
+				function success(response) {
+					$ctrl.newbooks = response.data;
+					$ctrl.dataLoaded = true;
+					loadBooks(19);
+					findWidth();
+				},
+				function error(response) {
+					console.log(response)
+				});
 
 
+			function loadBooks(n) {
+
+				let i = 1;
+
+				while (i <= n && loadCursor < $ctrl.newbooks.length) {
+					$ctrl.display[dispCursor] = $ctrl.newbooks[loadCursor]
+					i++;
+          loadCursor++;
+          dispCursor++;
+
+          if (loadCursor == $ctrl.newbooks.length) {
+            loadCursor = 0;
+          }
+
+				}
+
+				setTimeout(() => {
+					console.log(innerCar.children.length);
+				}, 0);
+			}
+
+      function findWidth() {
+						screenWidth = scrollCont.offsetWidth;
+						// console.log(screenWidth);
+					}
+
+      function setWidth() {
+						innerCar.style.width = String(innerCar.offsetWidth + 1000) + "px";
+						// console.log(innerCar.offsetWidth);
+					}
+
+      $ctrl.scrollLeft = function () {
+						scrollCont.scrollLeft -= 500;
+					};
+
+				$ctrl.scrollRight = function () {
+					scrollCont.scrollLeft += 500;
+
+					console.log(scrollCont.scrollLeft + screenWidth);
+					console.log(innerCar.offsetWidth);
+
+
+					if (scrollCont.scrollLeft + screenWidth >= innerCar.offsetWidth - 1000) {
+
+						loadBooks(8);
+
+						setTimeout(() => {
+							setWidth();
+						}, 0);
+
+					};
+
+
+					addEventListener('resize', (Event) => {
+
+						findWidth();
+
+					});
+
+
+				};
+
+			});
+  
 })();
