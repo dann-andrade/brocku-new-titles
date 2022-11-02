1f354045acfde4a9d4b7eaca384a8a5e08a7744f
dandrade <dandrade@github.com>
2022-11-02 19:08:04 +0000

    Display tweaks, removed troubleshooting logs statements

--- web/js/app.js
+++ web/js/app.js
@@ -1,80 +1,70 @@
-(function() {
+(function () {
 
-  'use strict';
+	'use strict';
 
-  angular
-    .module('newTitles', [])
-    .controller('MainController',
-		function ($http) {
+	angular
+		.module('newTitles', [])
+		.controller('MainController',
+			function ($http) {
 
-			const $ctrl = this;
-			const scrollCont = document.getElementById('bu-outer-carousel');
-			const innerCar = document.getElementById('bu-inner-carousel');
+				const $ctrl = this;
+				const scrollCont = document.getElementById('bu-outer-carousel');
+				const innerCar = document.getElementById('bu-inner-carousel');
 
-      scrollCont.scrollLeft = 0;
+				scrollCont.scrollLeft = 0;
 
-			var loadCursor = 0;
-      var dispCursor = 0;
-			var screenWidth = 0;
+				var loadCursor = 0;
+				var dispCursor = 0;
+				var screenWidth = 0;
 
-			$ctrl.newbooks = [];
-			$ctrl.display = [];
-			$ctrl.dataLoaded = false;
+				$ctrl.newbooks = [];
+				$ctrl.display = [];
+				$ctrl.dataLoaded = false;
 
-			$http.get('http://rtod.library.brocku.ca:8080/data/gtitles.json').then(
-				function success(response) {
-					$ctrl.newbooks = response.data;
-					$ctrl.dataLoaded = true;
-					loadBooks(19);
-					findWidth();
-				},
-				function error(response) {
-					console.log(response)
-				});
+				$http.get('http://rtod.library.brocku.ca:8080/data/gtitles.json').then(
+					function success(response) {
+						$ctrl.newbooks = response.data;
+						$ctrl.dataLoaded = true;
+						loadBooks(19);
+						findWidth();
+					},
+					function error(response) {
+						console.log(response)
+					});
 
 
-			function loadBooks(n) {
+				function loadBooks(n) {
 
-				let i = 1;
+					let i = 1;
 
-				while (i <= n && loadCursor < $ctrl.newbooks.length) {
-					$ctrl.display[dispCursor] = $ctrl.newbooks[loadCursor]
-					i++;
-          loadCursor++;
-          dispCursor++;
+					while (i <= n && loadCursor < $ctrl.newbooks.length) {
+						$ctrl.display[dispCursor] = $ctrl.newbooks[loadCursor]
+						i++;
+						loadCursor++;
+						dispCursor++;
 
-          if (loadCursor == $ctrl.newbooks.length) {
-            loadCursor = 0;
-          }
+						if (loadCursor == $ctrl.newbooks.length) {
+							loadCursor = 0;
+						}
+
+					}
 
 				}
 
-				setTimeout(() => {
-					console.log(innerCar.children.length);
-				}, 0);
-			}
+				function findWidth() {
+					screenWidth = scrollCont.offsetWidth;
+				}
 
-      function findWidth() {
-						screenWidth = scrollCont.offsetWidth;
-						// console.log(screenWidth);
-					}
+				function setWidth() {
+					innerCar.style.width = String(innerCar.offsetWidth + 1000) + "px";
+				}
 
-      function setWidth() {
-						innerCar.style.width = String(innerCar.offsetWidth + 1000) + "px";
-						// console.log(innerCar.offsetWidth);
-					}
-
-      $ctrl.scrollLeft = function () {
-						scrollCont.scrollLeft -= 500;
-					};
+				$ctrl.scrollLeft = function () {
+					scrollCont.scrollLeft -= 500;
+				};
 
 				$ctrl.scrollRight = function () {
-					scrollCont.scrollLeft += 500;
-
-					console.log(scrollCont.scrollLeft + screenWidth);
-					console.log(innerCar.offsetWidth);
-
-
+					
 					if (scrollCont.scrollLeft + screenWidth >= innerCar.offsetWidth - 1000) {
 
 						loadBooks(8);
@@ -85,16 +75,16 @@
 
 					};
 
-
-					addEventListener('resize', (Event) => {
-
-						findWidth();
-
-					});
-
+					scrollCont.scrollLeft += 500;
 
 				};
 
+				addEventListener('resize', (Event) => {
+
+					findWidth();
+
+				});
+
 			});
-  
+
 })();
