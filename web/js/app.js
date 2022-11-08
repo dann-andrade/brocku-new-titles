52062b30dab4f171923a246280a43c2f54153489
dandrade <dandrade@github.com>
2022-11-08 16:56:44 +0000

    Added comments

--- web/js/app.js
+++ web/js/app.js
@@ -11,6 +11,7 @@
 				const scrollCont = document.getElementById('bu-outer-carousel');
 				const innerCar = document.getElementById('bu-inner-carousel');
 
+				// Set scroll position to zero on component initilalization
 				scrollCont.scrollLeft = 0;
 
 				var loadCursor = 0;
@@ -22,6 +23,7 @@
 				$ctrl.display = [];
 				$ctrl.dataLoaded = false;
 
+				// Retrieve daily data, load 20 books and calculate width
 				$http.get('http://rtod.library.brocku.ca:8080/data/gtitles.json').then(
 					function success(response) {
 						$ctrl.newbooks = response.data;
@@ -33,55 +35,49 @@
 						console.log(response);
 					});
 
-
+				// Load n books from full array into display array, infinitely
 				function loadBooks(n) {
-
 					let i = 1;
-
 					while (i <= n && loadCursor < $ctrl.newbooks.length) {
 						$ctrl.display[dispCursor] = $ctrl.newbooks[loadCursor]
 						i++;
 						loadCursor++;
 						dispCursor++;
-
 						if (loadCursor == $ctrl.newbooks.length) {
 							loadCursor = 0;
-						}
+						};
+					};
+				};
 
-					}
-
-				}
-
+				// Retrieve viewport width, dynamically sets scroll distance
 				function findWidth() {
 					screenWidth = scrollCont.offsetWidth;
 					scrollWidth = (screenWidth > 500) ? 500 : screenWidth;
-					console.log(scrollWidth);
-				}
+				};
 
+				// Adds space to inner carousel container to make room for new items
 				function setWidth() {
 					innerCar.style.width = String(innerCar.offsetWidth + 1000) + "px";
-				}
+				};
 
+				// Scrolls left
 				$ctrl.scrollLeft = function () {
 					scrollCont.scrollLeft -= scrollWidth;
 				};
 
+				// Scrolls left, extends if near the end of loaded covers
 				$ctrl.scrollRight = function () {
-					
 					if (scrollCont.scrollLeft + screenWidth >= innerCar.offsetWidth - 1000) {
-
 						loadBooks(8);
-
 						setTimeout(() => {
 							setWidth();
 						}, 0);
-
 					};
-
 					scrollCont.scrollLeft += scrollWidth;
-
 				};
 
+				//Screen Resize Event
+      	//Calls the find width function to update the width
 				addEventListener('resize', (Event) => {
 					findWidth();
 				});
